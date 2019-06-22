f75e017cee140892fefc0a403ee0862913b05597
Jake Howard <git@theorangeone.net>
2019-06-22 22:10:15 +0100

    Init project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gitea-cgit-adapter
+
 Export public gitea repos in a way cgit understands
+
+## Installation
+
+1. `python -m venv env`
+2. `env/bin/pip install -e .`
+3. `env/bin/python run.py <gitea app.ini> <cgitrepos>
---
+++ run.py
@@ -0,0 +1,15 @@
+#!env/bin/python
+
+
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('gitea_config', type=argparse.FileType('r'))
+    parser.add_argument('output_file', type=argparse.FileType('w'))
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    print(get_args())
